ed952f4f00f9701529702a26b0ae10593e9264db
Cody Frank <cody.frank30@gmail.com>
2019-03-20 23:25:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+#prime number = number only divisable by 1 and itself greater then 1
+#2 is only prime number that is even.. even num / 2 has no remainder
+def prime?(integer)
+  if integer <= 1
+    return false
+  elsif integer == 2
+    return true
+  end
+  array = []
+  array=*(2..integer-1)
+  array.collect do |x|
+    integer % x
+    if array.include?(0) == true
+      puts "not a prime"
+      return false
+    else
+      puts integer
+      puts array.include?(0)
+      return true
+    end
+  end
+end
